e1855194fbae8c3ea55d3059e8294ec13b26f70f
mereillegabi <mereillegabrielle@gmail.com>
2026-08-21 10:22:55 +0100

    Create database and countries table

---
+++ SalesCet108.Web/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SalesCet108.Web.Data.Entities;
+
+namespace SalesCet108.Web.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Country> Countries { get; set; }
+    }
+}
--- SalesCet108.Web/Program.cs
+++ SalesCet108.Web/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using SalesCet108.Web.Data;
+
 namespace SalesCet108.Web
 {
     public class Program
@@ -8,6 +11,12 @@ namespace SalesCet108.Web
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
+
+            builder.Services.AddDbContext<DataContext>( o =>
+            {
+                o.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+            }
+                );
 
             var app = builder.Build();
 
